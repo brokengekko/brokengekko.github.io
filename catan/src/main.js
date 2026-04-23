e06da908bcc0bddbbe1a84f8eb18d0000b81680c
Jaya Prime <4504855+brokengekko@users.noreply.github.com>
2026-04-22 19:44:13 -0500

    Delete catan/src directory

--- catan/src/main.js
+++
@@ -1,9 +0,0 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-
-const App = require('./components/app.js');
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root'),
-);
